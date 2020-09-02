77eb4233bf9c058d06936b2acd4e208f0a436314
sgd122 <sgd122@users.noreply.github.com>
2020-09-02 07:11:33 +0000

    deploy: f02e244cc044e356873f52cc6919c968c187af16

---
+++ precache-manifest.a1fd7bc1f6d060b4f85ea64dc5c74bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051e21cbdfacfa6f109b8cc63ce802ec",
+    "url": "/doulit/index.html"
+  },
+  {
+    "revision": "122800f6247129300849",
+    "url": "/doulit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240cc2887a80d9d1284",
+    "url": "/doulit/static/js/2.b9768426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doulit/static/js/2.b9768426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122800f6247129300849",
+    "url": "/doulit/static/js/main.503bcdfa.chunk.js"
+  },
+  {
+    "revision": "5f91bd1fedd9a2b9d33e",
+    "url": "/doulit/static/js/runtime-main.46258946.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/doulit/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/doulit/precache-manifest.a1fd7bc1f6d060b4f85ea64dc5c74bf7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doulit/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
